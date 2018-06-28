e8d01119ce0bc3f2e3be43f172b198ae7ab4eef8
Jerry Yang <jyang9@nd.edu>
2018-06-27 22:22:01 -0400

    initial commit

---
+++ src/edu/nd/Main.java
@@ -0,0 +1,12 @@
+import edu.stanford.nlp.ie.util.RelationTriple;
+import edu.stanford.nlp.simple.*;
+
+
+package edu.nd;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
